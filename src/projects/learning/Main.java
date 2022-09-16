2fd6be3e1ca6b2165e22ca1943616930aadbbc80
scaulfield7 <scaulfield@live.ie>
2022-09-16 11:15:14 +0100

    Initial commit

---
+++ src/projects/learning/Main.java
@@ -0,0 +1,27 @@
+package projects.learning;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char letter = 'D';
+	    switch(letter){
+            case 'A':
+                System.out.println("The letter is A.");
+                break;
+            case 'B':
+                System.out.println("The letter is B.");
+                break;
+            case 'C':
+                System.out.println("The letter is C.");
+                break;
+            case 'D':
+                System.out.println("The letter is D.");
+                break;
+            case 'E':
+                System.out.println("The letter is E.");
+                break;
+            default:
+                System.out.println("Letter not found.");
+        }
+    }
+}
